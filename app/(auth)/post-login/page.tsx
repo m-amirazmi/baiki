b51aab39b51484ea0cc8a03deed4f805bd49201a
Muhamad Amir <m.amirazmi@hotmail.com>
2025-09-05 12:09:09 +0800

    Initialize turborepo

--- app/(auth)/post-login/page.tsx
+++
@@ -1,5 +0,0 @@
-import PostLoginRedirect from "@/components/post-login-redirect";
-
-export default function PostLogin() {
-  return <PostLoginRedirect />;
-}
--- app/platform/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function PlatformDashboardPage() {
-  return <div>Hi there dashboard here</div>;
-}
