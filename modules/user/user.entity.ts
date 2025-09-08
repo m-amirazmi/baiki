3bdab312e61aae38fc904c14461370f4b787b584
Muhamad Amir <amir.azmi@compasia.com>
2025-09-08 16:07:57 +0800

    Yes

--- modules/user/user.entity.ts
+++ modules/user/user.entity.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const createUserSchema = z.object({
   name: z.string().min(2),
   email: z.string().email().optional(),
+
   password: z.string().min(6).optional(), // optional if Google only
   role: z.enum(["admin", "technician", "store"]),
   tenantId: z.string().uuid(),
