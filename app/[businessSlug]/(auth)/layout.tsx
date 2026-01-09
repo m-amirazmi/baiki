0593331743c57eaeeb8b86f170b0b73555568671
Muhamad Amir <m.amirazmi@hotmail.com>
2026-01-10 02:12:07 +0800

    feat(auth): enhance BusinessLoginLayout with background image and branding elements

--- app/[businessSlug]/(auth)/layout.tsx
+++ app/[businessSlug]/(auth)/layout.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+
 export default function BusinessLoginLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <main className="relative">
+      <div className="absolute top-0 left-0 w-screen h-screen -z-10 overflow-hidden">
+        <Image src="http://baiki.test/images/signup.webp" alt="Signup" fill />
+      </div>
+      <div className="bg-app-brand-white/70 backdrop-blur-xl w-1/2 h-screen flex flex-col justify-center items-center">
+        <div className="flex items-center gap-4">
+          <Image
+            src="http://baiki.test/images/logo.webp"
+            alt="Signup"
+            width={50}
+            height={50}
+          />
+          <p className="text-3xl">Baiki RMS</p>
+        </div>
+        {children}
+      </div>
+    </main>
+  );
 }
